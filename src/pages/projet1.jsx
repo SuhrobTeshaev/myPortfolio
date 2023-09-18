50bde66f07814b2738a9c99bb4ea0ab438c450b5
SuhrobTeshaev <125867321+SuhrobTeshaev@users.noreply.github.com>
2023-09-18 18:35:28 +0700

    commit new Project

--- src/pages/projet1.jsx
+++ src/pages/projet1.jsx
@@ -4,30 +4,31 @@ import BtnGitHub from '../components/gitHub/BtnGitHub';
 import { projects } from '../components/helpers/projectList';
 
 const Project1 = () => {
-    const   {id} = useParams();
+    const { id } = useParams();
     const project = projects[id];
-      
-    return ( 
-        <main className="section">
-        <div className="container">
-            <div className="project-details">
 
-                <h1 className="title-1">{project.title}</h1>
+    return (
+        <main className="section">
+            <div className="container">
+                <div className="project-details">
+
+                    <h1 className="title-1">{project.title}</h1>
 
                     <p>{id}</p>
 
-                <img src={project.imgBig} alt={project.title} className="project-details__cover"/>
+                    <img src={project.imgBig} alt={project.title} className="project-details__cover" />
 
-                <div className="project-details__desc">
-                    <p>{project.skils}</p>
+                    <div className="project-details__desc">
+                        <p>{project.skils}</p>
+                    </div>
+
+                    {project.gitHubLink && <BtnGitHub link={project.gitHubLink} />}
+
+
                 </div>
-                {project.gitHubLink && <BtnGitHub link={project.gitHubLink}/>}
-               
-
             </div>
-        </div>
-    </main>
-     );
+        </main>
+    );
 }
- 
+
 export default Project1;
